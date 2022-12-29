dfee477bcbd3b9637c045877c6fd6900afbf8cb0
Yashodha Lakshana <yashodhalakshana@gmail.com>
2022-12-29 23:55:16 +0530

    Register activity updated

--- app/src/main/java/com/example/ee4354_g9_dr_doo/Activity_Inside_Register.java
+++ app/src/main/java/com/example/ee4354_g9_dr_doo/Activity_Inside_Register.java
@@ -2,13 +2,92 @@ package com.example.ee4354_g9_dr_doo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.ee4354_g9_dr_doo.databinding.ActivityMainTabBinding;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Activity_Inside_Register extends AppCompatActivity {
-
+    private int kindInt =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inside_register);
+        Button btnMap = findViewById(R.id.btnSetOnMap);
+        Button btnReg = findViewById(R.id.btnRegister);
+        TextView textHeading = findViewById(R.id.txtHeadingRegView);
+        EditText editName = findViewById(R.id.editName);
+        EditText editContact = findViewById(R.id.editContact);
+        EditText editDate = findViewById(R.id.editDate);
+        EditText editRewards = findViewById(R.id.editRewards);
+        EditText editRatings = findViewById(R.id.editRating);
+        EditText editLocation = findViewById(R.id.editLocation);
+
+
+        Bundle extras = getIntent().getExtras();
+        kindInt = extras.getInt("Kind");
+        switch (kindInt){
+            case 1:
+                textHeading.setText("New Animal To Rescue");
+                break;
+            case 2:
+                textHeading.setText("Find My Pet");
+                break;
+            case 3:
+                textHeading.setText("Add a Pet Shop");
+                break;
+            case 4:
+                textHeading.setText("Add a Veterinarian");
+                break;
+        }
+
+        FloatingActionButton fab = findViewById(R.id.fabAddImage);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // code to select image
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/ee4354_g9_dr_doo/MainTabActivity.java
+++ app/src/main/java/com/example/ee4354_g9_dr_doo/MainTabActivity.java
@@ -1,11 +1,13 @@
 package com.example.ee4354_g9_dr_doo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -24,7 +26,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 public class MainTabActivity extends AppCompatActivity {
 
     private ActivityMainTabBinding binding;
-
+    private int currentPosition=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,21 +34,24 @@ public class MainTabActivity extends AppCompatActivity {
         binding = ActivityMainTabBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         FloatingActionButton fab = binding.fab;
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(adapter);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Im on tab " + currentPosition, Snackbar.LENGTH_LONG)
+                       // .setAction("Action", null).show();
+                Intent intent =new Intent(getApplicationContext(),Activity_Inside_Register.class);
+                intent.putExtra("Kind",currentPosition+1);
+                startActivity(intent);
             }
         });
 
-        //< get elements >
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
-        //</ get elements >
 
-        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
-        viewPager2.setAdapter(adapter);
 
         new TabLayoutMediator(tabLayout, viewPager2,
                 new TabLayoutMediator.TabConfigurationStrategy() {
@@ -70,6 +75,22 @@ public class MainTabActivity extends AppCompatActivity {
                     }
                 }).attach();
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                currentPosition = tab.getPosition();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Nothing to do
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Nothing to do
+            }
+        });
     }
 
 
